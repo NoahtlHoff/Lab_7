f174bb591968dedb3dbc7f9011531586117c08f2
NoahH Laptop <NoahH@HPLAPTOP>
2024-11-02 14:40:43 +0100

    Add project files.

---
+++ Lab_7/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    internal class Employee
+    {
+        //Egenskaper
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public decimal Salary { get; set; }
+
+        //Konstruktor
+        public Employee(int id, string name, string gender, decimal salary)
+        {
+            Id = id;
+            Name = name;
+            Gender = gender;
+            Salary = salary;
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Gender: {Gender}, Salary: {Salary}";
+        }
+    }
+}
---
+++ Lab_7/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Lab_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new List<Employee>
+            {
+                new Employee(1, "Alice", "Female", 50000),
+                new Employee(2, "Bob", "Female", 75000),
+                new Employee(3, "Robert", "Male", 35000),
+                new Employee(4, "Anna", "Female", 40000),
+                new Employee(5, "Noah", "Male", 150000),
+            };
+
+
+            Stack<Employee> employeeStack = new Stack<Employee>();
+            Console.WriteLine("Alla anställda:");
+            foreach (var employee in employees)
+            {
+                employeeStack.Push(employee);
+                Console.WriteLine(employee);
+                Console.WriteLine($"Antal i stacken: {employeeStack.Count}");
+            }
+
+            Console.WriteLine();
+
+            foreach (var employee in employees)
+            {
+                employeeStack.Pop();
+                Console.WriteLine(employee);
+                Console.WriteLine($"Antal i stacken: {employeeStack.Count}");
+            }
+
+            foreach (var employee in employees)
+            {
+                employeeStack.Push(employee);
+            }
+        }
+    }
+}
